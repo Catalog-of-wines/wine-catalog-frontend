61b530613252ca67e860f181f987ad22ee0da2b5
Andrii-Kuzmenko <andriikuzmenko35@gmail.com>
2023-06-28 15:01:49 +0300

    add classNames mixin-hover button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,12 +4,12 @@
 import { DetailedHTMLProps } from "react";
 
 interface Props extends DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-  // props: string,
-  children: React.ReactNode | string
+  onClick: () => void;
+  children: React.ReactNode | string;
 }
 
-export const Button: React.FC<Props> = ({ children, ...props }) => (
-  <button {...props}>
+export const Button: React.FC<Props> = ({ children, onClick, ...props }) => (
+  <button type='button' onClick={onClick} {...props}>
     {children}
   </button>
 );
