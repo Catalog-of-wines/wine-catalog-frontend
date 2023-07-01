c8c6fbdb31bc0f30cbf4c6086dec080e52eed0d2
ShkarupaDenys <shkarupadenys@gmail.com>
2023-07-01 22:55:41 +0300

    Filter categories was implemented.

--- src/components/Radio/Radio.tsx
+++ src/components/Radio/Radio.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import styles from './Radio.module.scss';
+import { OnChange } from '../../types/events';
 
 export interface RadioProps {
   id: string;
@@ -7,7 +8,8 @@ export interface RadioProps {
   checked?: boolean;
   label?: string;
   name: string;
-  onChange: (value: string, name: string) => void;
+  categoryName?: string;
+  onChange: (e: OnChange) => void;
 }
 
 export const Radio: FC<RadioProps> = ({
@@ -16,6 +18,7 @@ export const Radio: FC<RadioProps> = ({
   disabled,
   checked,
   name,
+  categoryName,
   onChange,
 }) => (
   <label className={styles.radio}>
@@ -27,7 +30,8 @@ export const Radio: FC<RadioProps> = ({
       className={styles.real}
       disabled={disabled}
       checked={checked}
-      onChange={() => onChange(id, name)}
+      onChange={onChange}
+      data-category={categoryName}
     />
     <div className={styles.custom} />
     <span className={styles.label}>{label}</span>
--- src/components/SideMenu/components/CategoriesList/CategoriesList.tsx
+++ src/components/SideMenu/components/CategoriesList/CategoriesList.tsx
@@ -1,37 +1,48 @@
 import { FC } from 'react';
 import { Checkbox, Radio } from '../../../index';
 import styles from './CategoriesList.module.scss';
+import { OnChange } from '../../../../types/events';
 
 export interface CategoriesListProps {
+  categoryName: string;
   list: string[];
   type: 'radio' | 'checkbox';
   name?: string;
+  selectedCategories: string[];
+  handleOnChange: (e: OnChange) => void;
 }
 
 export const CategoriesList: FC<CategoriesListProps> = ({
+  categoryName,
   list,
   type,
   name = '',
+  selectedCategories,
+  handleOnChange,
 }) => (
   <ul className={styles.categoriesList}>
     {type === 'checkbox'
       ? list.map((category) => (
           <li className={styles.categoriesItem} key={category}>
             <Checkbox
+              checked={selectedCategories.includes(category)}
               id={category}
               label={category}
               name={category}
-              onChange={() => {}}
+              onChange={handleOnChange}
+              categoryName={categoryName}
             />
           </li>
         ))
       : list.map((category) => (
           <li className={styles.categoriesItem} key={category}>
             <Radio
+              checked={selectedCategories.includes(category)}
               id={category}
               label={category}
               name={name}
-              onChange={() => {}}
+              onChange={handleOnChange}
+              categoryName={categoryName}
             />
           </li>
         ))}
