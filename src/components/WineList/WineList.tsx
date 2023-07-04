737b3ef57308b63ebcafd42061fda4da51ac10db
Andrii-Kuzmenko <andriikuzmenko35@gmail.com>
2023-07-04 19:44:34 +0300

    renamed wine id

--- src/components/WineList/WineList.tsx
+++ src/components/WineList/WineList.tsx
@@ -10,8 +10,8 @@ export const WineList = React.memo<Props>(({wines}) => (
     <>
       {wines.map((wine) => (
         <WineCard
-          key={wine._id}
-          wineId={wine._id}
+          key={wine.id}
+          wineId={wine.id}
           name={wine.name}
           price={wine.price}
           image={wine.image_url}
