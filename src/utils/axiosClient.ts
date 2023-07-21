24d96ee896f0e56d799f88d1fdc1253654f5a109
Andrii-Kuzmenko <andriikuzmenko35@gmail.com>
2023-07-21 16:25:23 +0300

    fix: axiosClient

--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -1,5 +1,7 @@
 // // import axios from 'axios';
-// import { BASE_URL } from './fetchClient';
+import { BASE_URL } from './fetchClient';
+console.log(BASE_URL);
+
 
 // const instance = axios.create({
 //   baseURL: BASE_URL,
