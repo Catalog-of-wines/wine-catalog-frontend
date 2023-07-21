64d7c4a1f6b24c49c4bde2a0ee00655030194d14
Andrii-Kuzmenko <andriikuzmenko35@gmail.com>
2023-07-21 11:17:14 +0300

    add: authSlice to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import activeWineListReducer from '../features/activeWineList/activeWineListSlice';
 import selectedWineReducer from '../features/selectedWine/selectedWineSlice';
 import favoritesReducer from '../features/favorites/favoritesSlice';
+import authReducer from '../features/auth/authSlice';
 
 export const store = configureStore({
   reducer: {
     activeWineList: activeWineListReducer,
     selectedWine: selectedWineReducer,
     favorites: favoritesReducer,
+    auth: authReducer,
   },
 });
 
