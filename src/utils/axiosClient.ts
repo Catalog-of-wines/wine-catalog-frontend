97a4b260247f50a361a87b257b59d7fe3371a1a4
Andrii-Kuzmenko <andriikuzmenko35@gmail.com>
2023-07-21 16:16:40 +0300

    prepared: axiosClient

---
+++ src/utils/axiosClient.ts
@@ -0,0 +1,19 @@
+// // import axios from 'axios';
+// import { BASE_URL } from './fetchClient';
+
+// const instance = axios.create({
+//   baseURL: BASE_URL,
+// });
+
+// const addAccessToken = (accessToken: string) => {
+//   instance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+// };
+
+// export const client = {
+//   async post<T>(url: string, data: any, accessToken:string) {
+//     addAccessToken(accessToken);
+//     const response = await instance.post<T>(url, data);
+
+//     return response.data;
+//   },
+// };
