133ef423f82b6b3e22e999cf95b0ec3db6599001
Andrii-Kuzmenko <andriikuzmenko35@gmail.com>
2023-08-06 12:23:06 +0300

    add: form validation

--- src/pages/AboutUsPage/dataAboutUs.ts
+++ src/pages/AboutUsPage/dataAboutUs.ts
@@ -15,11 +15,6 @@ export const aboutUsData = [
     url: 'https://www.linkedin.com/in/andrii-kuzmenko-65a775262/',
   },
   {
-    name: 'Denys Shkarupa',
-    role: '(Frontend)',
-    url: 'https://www.linkedin.com/in/shkarupadenys?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAB_3hPgBJJdli5kEiQGm7lveVx-1S71Ve24&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3BcewMZXLSQSikBspauv95CQ%3D%3D',
-  },
-  {
     name: 'Ірина Кравцова',
     role: '(UX-UI)',
     url: 'https://www.linkedin.com/in/irina-kravtsova-004278261/',
