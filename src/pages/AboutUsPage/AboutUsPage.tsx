0e8d987b4394a39bc5a1a86d9ebed36db197376a
Andrii-Kuzmenko <andriikuzmenko35@gmail.com>
2023-09-17 08:55:36 +0300

    update: aboutUsPage

--- src/pages/AboutUsPage/AboutUsPage.tsx
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -1,16 +1,28 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { SmallPageTitle } from "../../components";
-import { LinkedInIcon } from "../../components/icons";
-import { aboutUsData } from "./dataAboutUs";
+// import { LinkedInIcon } from "../../components/icons";
+// import { aboutUsData } from "./dataAboutUs";
 import styles from './AboutUsPage.module.scss';
 
 export const AboutUsPage: React.FC = () => (
   <div className={styles.aboutUs}>
     <div className={styles.titleGroup}>
-      <LinkedInIcon />
-      <SmallPageTitle className={styles.title}>LinkedIn</SmallPageTitle>
+      {/* <LinkedInIcon /> */}
+      {/* <SmallPageTitle className={styles.title}>LinkedIn</SmallPageTitle> */}
+      <SmallPageTitle className={styles.title}>
+      Якщо вам потрібно підібрати вино під конкретну закуску 
+      (наприклад, під білу рибу, дичину чи десерти), 
+      або знайти вино за певним ароматом (наприклад, з нотками меду, тютюну чи горіхів)
+      </SmallPageTitle>
+      <SmallPageTitle className={styles.title}>
+        - You are welcome!
+      </SmallPageTitle>
+      <SmallPageTitle className={styles.title}>
+      WineBox – це каталог вин, де ви зможете здійснити пошук серед двох тисяч видів вин, 
+      які продаються в Україні і мають докладний опис.
+      </SmallPageTitle>
     </div>
-    <ul>
+    {/* <ul>
       {aboutUsData.map(contact =>
         <li className={styles.listItem} key={contact.url}>
           <Link
@@ -23,6 +35,6 @@ export const AboutUsPage: React.FC = () => (
           </Link>
         </li>
       )}
-    </ul>
+    </ul> */}
   </div>
 )
